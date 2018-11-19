2c6dcd8285c4d322d7207e97b5317bbb53a73c05
Alexandre Trindade <alexandredct@gmail.com>
2018-11-19 19:53:14 -0200

    Organização do projeto

---
+++ src/java_4_aula1_atividade1/Fluxo.java
@@ -0,0 +1,24 @@
+package java_4_aula1_atividade1;
+
+public class Fluxo {
+	public static void main(String[] args) {
+		System.out.println("Início do main");
+		metodo1();
+		System.out.println("Fim do main");
+	}
+	private static void metodo1() {
+		System.out.println("Início do método 1");
+		metodo2();
+		System.out.println("Fim do metodo 1");
+	}
+	
+	private static void metodo2() {
+		System.out.println("Início do método 2");
+		
+		for (int i=1; i<= 5; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Fim do metodo 2");
+	}
+}
