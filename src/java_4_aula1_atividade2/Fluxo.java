306c0994acc1d23095485124dff6b85b534b0e94
Alexandre Trindade <alexandredct@gmail.com>
2018-12-03 20:47:44 -0200

    .

--- src/java_4_aula1_atividade2/Fluxo.java
+++ src/java_4_aula1_atividade2/Fluxo.java
@@ -1,35 +1,24 @@
-package java_4_aula1_atividade1;
+package java_4_aula1_atividade2;
 
 public class Fluxo {
 	public static void main(String[] args) {
 		System.out.println("Início do main");
-		try {
-			metodo1();
-		} catch (ArithmeticException e) {
-			System.out.println("Exceção aritmética");
-			e.printStackTrace();
-		} catch (NullPointerException | ArrayStoreException e) {
-			e.printStackTrace();
-		}
+		metodo1();
 		System.out.println("Fim do main");
 	}
-
 	private static void metodo1() {
 		System.out.println("Início do método 1");
 		metodo2();
 		System.out.println("Fim do metodo 1");
 	}
-
+	
 	private static void metodo2() {
 		System.out.println("Início do método 2");
-
-		for (int i = 1; i <= 5; i++) {
+		
+		for (int i=1; i<= 5; i++) {
 			System.out.println(i);
-			//int a = i / 0;
-			String a = null;
-			System.out.println(a.length());
 		}
-
+		
 		System.out.println("Fim do metodo 2");
 	}
 }
